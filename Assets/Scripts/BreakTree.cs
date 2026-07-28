123d3fbeed182f57e007cf33c8432218c96866d4
Patrick Sheng <sheng.patrick@gmail.com>
2026-07-28 18:47:30 +1200

    feat: added a progress bar indicator for tree to show destroy progress

--- Assets/Scripts/BreakTree.cs
+++ Assets/Scripts/BreakTree.cs
@@ -5,17 +5,42 @@ public class BreakTree : Interactable
 {
 
     [SerializeField] private int health = 5;
-    
+    [SerializeField] private List<Sprite> treeSprites;
+    [SerializeField] private SpriteRenderer treeSpriteRenderer;
+
+    private int maxHealth;
+
+    void Start()
+    {
+        maxHealth = health;
+        treeSpriteRenderer.gameObject.SetActive(false);
+    }
+
     public override void Interact()
     {
-        if (health > 0)
+        if (health <= 0) return;
+
+        if (health == maxHealth)
         {
-            health--;
-            Debug.Log("Tree health: " + health);
+            treeSpriteRenderer.gameObject.SetActive(true);
         }
-        else
+
+        health--;
+        Debug.Log("Tree health: " + health);
+        UpdateSprite();
+
+        if (health <= 0)
         {
             Destroy(gameObject);
         }
+    }
+
+    private void UpdateSprite()
+    {
+        if (treeSprites == null || treeSprites.Count == 0) return;
+
+        int spriteIndex = (maxHealth - health) * (treeSprites.Count - 1) / maxHealth;
+        spriteIndex = Mathf.Clamp(spriteIndex, 0, treeSprites.Count - 1);
+        treeSpriteRenderer.sprite = treeSprites[spriteIndex];
     }
 }
